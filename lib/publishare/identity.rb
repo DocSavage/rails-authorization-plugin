d4e6c7ca6a21b2b8c501f618e201b35128f949f8
Kieran Pilkington <kieran@katipo.co.nz>
2008-10-01 17:23:18 +1300

    Allow users to run things like has_members and has_admins_or_members (and their boolean returning equivilents)

--- lib/publishare/identity.rb
+++ lib/publishare/identity.rb
@@ -101,11 +101,13 @@ module Authorization
         def method_missing( method_sym, *args )
           method_name = method_sym.to_s
           if method_name =~ /^has_(\w+)\?$/
-            role_name = $1.singularize
-            self.accepted_roles.find_all_by_name(role_name, :include => :users).any? { |role| role.users.compact.any? }
+            roles = $1.split('_or_').collect { |role| role.singularize }
+            roles = roles.flatten.compact
+            self.accepted_roles.find_all_by_name(roles, :include => :users).any? { |role| role.users.compact.any? }
           elsif method_name =~ /^has_(\w+)$/
-            role_name = $1.singularize
-            users = self.accepted_roles.find_all_by_name(role_name, :include => :users).collect { |role| role.users }
+            roles = $1.split('_or_').collect { |role| role.singularize }
+            roles = roles.flatten.compact
+            users = self.accepted_roles.find_all_by_name(roles, :include => :users).collect { |role| role.users }
             users.flatten.compact.uniq if users
           else
             super
