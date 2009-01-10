3d421fb6c4ada284d8a737df9d53acc9002c8f88
Lennart Melzer <lenni@lenni.lan>
2009-01-10 22:23:55 +0800

    Fixed the file naming to generate a "roles_user.rb" model instead of "role_user.rb"
    Signed-off-by: Glenn Rempe <glenn@rempe.us>

--- generators/role_model/role_model_generator.rb
+++ generators/role_model/role_model_generator.rb
@@ -14,7 +14,7 @@ class RoleModelGenerator < Rails::Generator::NamedBase
 
       # Model class, unit test, and fixtures.
       m.template 'role_model.rb',      File.join('app/models', class_path, "#{file_name}.rb")
-      m.template 'role_user_model.rb', File.join('app/models', class_path, "#{(file_name < 'user') ? "#{file_name}_user" : "user_#{file_name}"}.rb")
+      m.template 'role_user_model.rb', File.join('app/models', class_path, "#{(file_name.pluralize < 'user') ? "#{file_name}_user" : "user_#{file_name}"}.rb")
       m.template 'unit_test.rb',       File.join('test/unit', class_path, "#{file_name}_test.rb")
       m.template 'fixtures.yml',       File.join('test/fixtures', class_path, "#{table_name}.yml")
       
