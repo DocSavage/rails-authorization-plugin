a1c5cfd6263b46ae2523945516ec8a5f4c1474eb
Lennart Melzer <lenni@lenni.lan>
2009-01-10 21:23:37 +0800

    Fixed has_many .. :through relation for RoleUser model. It should now correctly infer RoleUser from the symbol in has_many :through and also allow the user to add relationship options to the relation (currently just for the join relation)
    Signed-off-by: Glenn Rempe <glenn@rempe.us>

--- generators/role_model/templates/role_model.rb
+++ generators/role_model/templates/role_model.rb
@@ -3,7 +3,7 @@
 # "moderator" for an instance of a model (i.e., an object), a model class,
 # or without any specification at all.
 class <%= class_name %> < ActiveRecord::Base
-  has_many :roles_users, :dependent => :delete_all
-  has_many :users, :through => :roles_users
+  has_many :role_users, :dependent => :delete_all
+  has_many :users, :through => :role_users
   belongs_to :authorizable, :polymorphic => true
 end
